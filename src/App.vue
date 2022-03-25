9a8445537e3e00b5691b910e851dd2285d4e1e35
Jared Meadows <jared.meadows@launchbadge.com>
2022-03-24 18:37:47 -0700

    caught up

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,7 @@
 			<ul class="todo-list">
 				<!-- These are here just to show the structure of the list items -->
 				<!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-				<li class="completed">
-					<div class="view">
-						<input class="toggle" type="checkbox" checked />
-						<label>Taste JavaScript</label>
-						<button class="destroy"></button>
-					</div>
-					<input class="edit" value="Create a TodoMVC template" />
-				</li>
-				<li>
-					<div class="view">
-						<input class="toggle" type="checkbox" />
-						<label>Buy a unicorn</label>
-						<button class="destroy"></button>
-					</div>
-					<input class="edit" value="Rule the web" />
-				</li>
+				<Todo v-for="(todo, index) of todoArray" :key="index" :todo="todo" />
 			</ul>
 		</section>
 		<!-- This footer should be hidden by default and shown when there are todos -->
@@ -49,14 +34,17 @@
 			<button class="clear-completed">Clear completed</button>
 		</footer>
 	</section>
+	{{ todoArray.length }}
 </template>
+
 <script setup lang="ts">
-import { ToDo } from "./domain/ToDo";
-const asdf: ToDo[] = [
-	{
-		checked: true,
-		message: "Hello world",
-	},
+import type { Todo } from "./domain/Todo";
+const todoArray: Todo[] = [
+	{ checked: true, message: "Stay Positive" },
+	{ checked: false, message: "Love Myself" },
+	{ checked: true, message: "Be Grateful" },
 ];
-console.log("hi from App.vue", asdf);
+const numbers = [1, 6, 4, 3, 23, 45, 76];
+let message: string = "hello world";
+console.log(message, 1, 5, numbers, todoArray);
 </script>
