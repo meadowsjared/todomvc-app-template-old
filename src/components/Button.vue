54a2301e7d8d432c6dee642190b096356ecd5a7e
Jared Meadows <meadowsjared@gmail.com>
2022-05-12 20:50:49 -0700

    created Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<a
+		:class="{ selected: filter === activeValue }"
+		@click="$emit('click', activeValue)"
+		href="#/"
+		>{{ label }}</a
+	>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	filter: string;
+	label: string;
+	activeValue: string;
+}>();
+
+defineEmits<{
+	(event: "click", value: string): void;
+}>();
+</script>
