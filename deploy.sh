097e3e1f54dda80f4a8b27e2a3c2c014dda29892
Jared Meadows <meadowsjared@gmail.com>
2023-01-14 20:53:48 -0800

    build: set up github pages
    Signed-off-by: Jared Meadows <meadowsjared@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'new deployment'
+git push -f git@github.com/meadowsjared/todomvc-app-template.git master:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/todomvc-app-template/'
+}
